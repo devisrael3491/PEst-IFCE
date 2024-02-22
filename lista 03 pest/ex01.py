99cf99749b58bbd0d8c54b2e1ea92a368402fed9
Israel Alves <152522126+devisrael349@users.noreply.github.com>
2024-02-22 15:16:57 -0300

    Delete lista 03 pest directory

--- lista 03 pest/ex01.py
+++
@@ -1,5 +0,0 @@
-count = 1
-while count <= 10:
-    print(count)
-    count += 1
-
--- lista 03 pest/ex03.py
+++
@@ -1,7 +0,0 @@
-num = 1
-soma = 0
-while num != 0:
-    num = int(input('insira um número ou 0 para parar: '))
-    soma += num
-
-print(f'a soma de todos os números é {soma}.')

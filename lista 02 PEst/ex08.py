3d2c21e1268777685569d2dd4949e79e48e3f554
Israel Alves <152522126+devisrael349@users.noreply.github.com>
2024-07-15 14:56:00 -0300

    Update ex08.py
    Correção de problemas lógicos

--- lista 02 PEst/ex08.py
+++ lista 02 PEst/ex08.py
@@ -1,9 +1,19 @@
-a = int(input('insira um número a: '))
-b = int(input('insira um número b: '))
-c = int(input('insira um número c: '))
-n = int(input('insira um número n: '))
+print(f'\n>>>Validando o teorema de fermat<<<\n')
 
-if n > 2 and ((a ** n + b ** n) == c ** n):
-    print('teorema violado.')
+
+a = int(input('insira o valor de a: '))
+b = int(input('insira o valor de b: '))
+c = int(input('insira o valor de c: '))
+n = int(input('insira o valor de n: '))
+
+
+if n > 2:
+    if a > 0 and b>0 and c>0:
+        if (a**n + b ** n == c ** n):
+            print(f'O teorema de Fermat foi violado!')
+        else:
+            print(f'O teorema de Fermat permanece de pé!')
+    else:
+        print(f'Os valores de a,b e c devem ser maiores que 0.')
 else:
-    print('teorema não violado.')
+    print(f'Para a validação ocorrer é necessário que n seja maior que 2.')

a1e3df93a9b47dce841eaf7d2db07e69b18812a4
Israel Alves <152522126+devisrael349@users.noreply.github.com>
2024-07-15 10:45:16 -0300

    Update ex02.py
    Adicionando a verificação de nota válida e inválida.

--- lista 02 PEst/ex02.py
+++ lista 02 PEst/ex02.py
@@ -1,10 +1,13 @@
 nota = float(input('insira uma nota: '))
 
-if nota >= 9:
-    print('A')
-elif nota >= 7:
-    print('B')
-elif nota >= 5:
-    print('C')
+if nota >= 0 and nota <= 10:
+    if nota >= 9:
+        print('A')
+    elif nota >= 7:
+        print('B')
+    elif nota >= 5:
+        print('C')
+    else:
+        print('D')
 else:
-    print('D')
+    print(f'{nota} não é uma nota válida.')

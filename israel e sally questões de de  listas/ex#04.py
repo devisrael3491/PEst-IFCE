012ce5d0c0e5ac20edf9d1d62bddc414a7351410
Israel Alves <152522126+devisrael349@users.noreply.github.com>
2024-08-12 15:33:34 -0300

    Update ex#04.py

--- israel e sally questões de de  listas/ex#04.py
+++ israel e sally questões de de  listas/ex#04.py
@@ -1,6 +1,6 @@
-numeros = [1,2,5,7,13, 16, 24, 8, 42, 1989]
+numeros = [1, 16, 24, 8, 42, 1989]
 
-for numero in numeros:
+for numero in numeros[:]:
     if numero % 2 == 0:
         numeros.remove(numero)
 

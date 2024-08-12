96e063996d4a7c51d6430ba71bb5233bb4bcba22
Israel Alves <152522126+devisrael349@users.noreply.github.com>
2024-08-12 15:35:00 -0300

    Update ex#05.py

--- israel e sally questões de de  listas/ex#05.py
+++ israel e sally questões de de  listas/ex#05.py
@@ -1,6 +1,6 @@
-cidades = ['maranguape','fortaleza', 'são paulo', 'rio de janeiro', 'brasilia', 'porto alegre']
+cidades = ['maranguape','fortaleza', 'são paulo', 'rio de janeiro', 'brasilia', 'porto alegre', 'nova york']
 
-cidade = str(input('insira o nome de uma cidade: '))
+cidade = str(input('insira o nome de uma cidade: ')).lower()
 
 if cidade in cidades:
     print(f'{cidade} existe na lista de cidades!')

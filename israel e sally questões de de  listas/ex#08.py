940cd5de612586a74038bd5e6fabe71ebd7d625e
Israel Alves <152522126+devisrael349@users.noreply.github.com>
2024-08-12 12:34:23 -0300

    corrigindo erro de lógica

--- israel e sally questões de de  listas/ex#08.py
+++ israel e sally questões de de  listas/ex#08.py
@@ -1,35 +1,16 @@
-numeros = []
-
-def verificar_se_eh_maior(num : int, lista : list):
-    for numero in lista:
-        if numero > num:
-            return False
-    return True
-
-def encontrar_pos_ideal(num : int, lista : list):
-
-    tamanho = len(lista)
-
+def encontra_pos_ideal(numero, numeros : list):
+    tamanho = len(numeros)
     for i in range(tamanho):
-        numero_atual = lista[i]
+        if numeros[i] >= numero:
+            return i
+    return tamanho
 
-        
-
-        
-
+numeros = []
 
 while True:
     numero = int(input('insira um número: '))
-    if len(numeros) == 0:
-        numeros.append(numero)
-    else:
-        tamanho = len(numeros)
-        for i in range(tamanho):
-            if (numeros[i] > numero) and (i != tamanho -1):
-                numeros.insert(i, numero)
-                break
-            elif (i != tamanho - 1):
-                numeros.append(numero)
+    pos = encontra_pos_ideal(numero, numeros)
+    numeros.insert(pos, numero)
 
     cont = str(input('deseja continuar? [s/n] ')).lower()
     while cont != 's' and cont != 'n':
@@ -38,9 +19,5 @@ while True:
     if cont == 'n':
         break
 
-
-    
-    
-
-
 print(numeros)
+
